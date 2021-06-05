7db6a55a76b5112b9d8ea4b972c96f4c6fde5d59
V Zhevaha <v.zhevaga@gmail.com>
2021-06-05 08:41:39 +0300

    fixed gallery and design filters

--- app/js/main.js
+++ app/js/main.js
@@ -4,10 +4,19 @@ $(function(){
 		dots: true,
 		arrows: false,
 		fade: true,
-		// autoplay: true,
+		autoplay: true,
 		autoplaySpeed: 2500
 	});
 
+	let container_gallery = document.querySelector('[data-ref="gallery"]');
+  let container_design = document.querySelector('[data-ref="design"]');
 
-		let mixer = mixitup('.gallery__content');
+	let config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+		let mixer_gallery = mixitup(container_gallery, config);
+		let mixer_design = mixitup(container_design, config);
 });
